c642b83bb3db4f24453351c30bd3a33f4c9765ad
Cassandra Comar <cass@ndra.io>
2025-09-11 16:15:43 -0400

    try removing gcmh-mode hook

--- init.el
+++ init.el
@@ -200,3 +200,4 @@
 ;; (setq inhibit-automatic-native-compilation nil)
 ;; (after! (doom-packages straight)
 ;;   (setq straight--native-comp-available t))
+(remove-hook 'doom-first-buffer-hook #'gcmh-mode)
