a47d70d418cf750f361a867ab3742eaefaefa1eb
Cassandra Comar <cass@ndra.io>
2025-09-22 11:56:28 -0400

    update semel

--- packages.el
+++ packages.el
@@ -114,4 +114,4 @@
 (package! sideline)
 (package! sideline-flymake)
 (package! eglot :recipe (:host github :repo "cassandracomar/eglot") :pin "b16e1cb02f6c6168b22301beeb84e4c19f508abb")
-(package! semel :recipe (:host github :repo "eshelyaron/semel") :pin "a077ca4fb54529f4d789aad209b05e424b2bee85")
+(package! semel :recipe (:host github :repo "eshelyaron/semel") :pin "d41d37c21efe1ba3a906ebca2d5611fcb5e84f5c")
