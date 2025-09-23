4b2b54237a5e3c45df5b5f8642f510d489b25052
Cassandra Comar <cass@ndra.io>
2025-09-23 14:49:57 -0400

    just set email address to cass@nie.rs for now

--- config.el
+++ config.el
@@ -6,7 +6,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Cassandra Comar"
-      user-mail-address "cass@ndra.io")
+      user-mail-address "cass@nie.rs")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
