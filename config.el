5d5c28b8e59dc29fc7695619d8a62394d7e473b4
Cassandra Comar <cass@ndra.io>
2025-09-23 12:44:34 -0400

    lower treesit-font-lock-level

--- config.el
+++ config.el
@@ -612,8 +612,9 @@
   :config
   (add-hook 'haskell-ts-mode-hook #'lsp!)
   (add-to-list 'eglot-server-programs '(haskell-ts-mode "haskell-language-server-wrapper" "-d" "lsp"))
+  (setq-local treesit-font-lock-level 3)
   :custom
-  (haskell-ts-font-lock-level 4)
+  (haskell-ts-font-lock-level 3)
   (haskell-ts-ghci "ghci")
   (haskell-ts-use-indent t))
 (add-hook! haskell-ts-mode 'prettify-symbols-mode)
