2078d9eef7d637bf6985e6b5dd7e8842f9344178
Cassandra Comar <cass@ndra.io>
2025-09-25 20:03:06 -0400

    go back to undo-tree
    vundo just stops at every branch

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
        electric          ; smarter, keyword-based electric-indent
        (ibuffer +icons)         ; interactive buffer management
        vc                ; version-control and Emacs, sitting in a tree
-       undo
+       (undo +tree)
 
        :term
        eshell            ; a consistent, cross-platform shell (WIP)
