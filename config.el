2d363a1fdcccfcae3b9380e3115d96ed289db8e8
Cassandra Comar <cass@ndra.io>
2025-12-05 18:06:22 -0500

    don't set defaultLibrary face
    it hits too much

--- config.el
+++ config.el
@@ -260,7 +260,8 @@ and set them for all frames (including the defaults for new frames)."
   ;; (eglot-semantic-async-face ((t (:inherit font-lock-preprocessor-face))))
   ;; (eglot-semantic-modification-face ((t (:inherit font-lock-function-name-face))))
   ;; (eglot-semantic-documentation-face ((t (:inherit font-lock-doc-face))))
-  (eglot-semantic-defaultLibrary-face ((t (:inherit font-lock-preprocessor-face)))))
+  ;; (eglot-semantic-defaultLibrary-face ((t (:inherit font-lock-preprocessor-face))))
+  )
 
 ;; just force a rebuild
 
