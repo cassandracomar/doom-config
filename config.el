cb0ddbbb482ba2963e6a9157148bfa802c17b022
Cassandra Comar <cass@mountclare.net>
2026-06-18 09:22:09 -0400

    enable gcmh-mode

--- config.el
+++ config.el
@@ -115,6 +115,7 @@
 ;;               (add-hook 'after-focus-change-function
 ;;                         (lambda ()
 ;;                           (igc-collect))))))
+(gcmh-mode)
 
 ;; ENVIRONMENT
 (defun +gpgconf-set-agent-env ()
